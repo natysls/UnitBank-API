f4e252a33157565e7305fd883b776d3ec64f693e
Uezili <weslmaciel@gmail.com>
2023-10-16 19:55:31 -0300

    Removendo import não usado.

--- src/main/java/com/uece/banking/uecebanking/repository/ContaRepository.java
+++ src/main/java/com/uece/banking/uecebanking/repository/ContaRepository.java
@@ -1,7 +1,5 @@
 package com.uece.banking.uecebanking.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +7,7 @@ import com.uece.banking.uecebanking.dto.Conta;
 
 @Repository
 public interface ContaRepository extends JpaRepository<Conta, Long> {
-    
+
     // Métodos personalizados, se necessário
     Conta findByNumeroConta(String numeroConta);
 }
